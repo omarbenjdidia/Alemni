7f17ff25ca88f094404af8fd5516fa87a2dca7c9
omarbenjdidia <benjdidia.omar@esprit.tn>
2023-11-13 20:41:14 +0100

    another front iheb

---
+++ Front/lib/utils.dart
@@ -0,0 +1,5 @@
+//String baseUrlWithoutHttp = "192.168.1.13:9090";
+import 'dart:io';
+
+//String baseUrlWithoutHttp =Platform.isAndroid ? '127.0.0.1:3000' : 'localhost:3000';
+String baseUrlWithoutHttp = "10.0.2.2:8000";
